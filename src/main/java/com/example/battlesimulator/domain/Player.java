7eac8db5e078d4cdd6bd342813c1b780cfeed210
Sxulless <User@ASUS-LAPTOP>
2025-03-12 20:01:00 +0100

    Player expansion

--- src/main/java/com/example/battlesimulator/domain/Player.java
+++ src/main/java/com/example/battlesimulator/domain/Player.java
@@ -11,15 +11,96 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-//    private int strength;
-//    private int agility;
-//    private int speed;
-//    private int stamina;
+    private int strength;
+    private int agility;
+    private int speed;
+    private int stamina;
     private int health;
-//    private int willpower;
-//    private int perception;
+    private int willpower;
+    private int perception;
     private int baseAttack;
 //    private int baseDefense;
 //    private int healthPoint;
 //    private int fatiguePoint;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getBaseAttack() {
+        return baseAttack;
+    }
+
+    public void setBaseAttack(int baseAttack) {
+        this.baseAttack = baseAttack;
+    }
+
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getWillpower() {
+        return willpower;
+    }
+
+    public void setWillpower(int willpower) {
+        this.willpower = willpower;
+    }
+
+    public int getPerception() {
+        return perception;
+    }
+
+    public void setPerception(int perception) {
+        this.perception = perception;
+    }
 }
