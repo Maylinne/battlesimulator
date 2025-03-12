1dda517c638dfd405b0e14e84b282947d3a5c3e7
Sxulless <User@ASUS-LAPTOP>
2025-03-12 18:59:13 +0100

    Initial commit

---
+++ src/test/java/com/example/battlesimulator/BattlesimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battlesimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlesimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
